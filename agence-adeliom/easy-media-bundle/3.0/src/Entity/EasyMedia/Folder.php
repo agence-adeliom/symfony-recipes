c515777aad1734288ab3348a22471584984884a9
JeanDavidDaviet <jeandaviddaviet@gmail.com>
2025-03-14 18:11:59 +0100

    copy the agence-adeliom/easy-media-bundle recipe to 3.0

---
+++ agence-adeliom/easy-media-bundle/3.0/src/Entity/EasyMedia/Folder.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Entity\EasyMedia;
+
+use Adeliom\EasyMediaBundle\Entity\Folder as BaseFolder;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity]
+#[ORM\Table(name: 'easy_media__folder')]
+class Folder extends BaseFolder
+{
+}
---
+++ agence-adeliom/easy-media-bundle/3.0/src/Entity/EasyMedia/Media.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Entity\EasyMedia;
+
+use Adeliom\EasyMediaBundle\Entity\Media as BaseMedia;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity]
+#[ORM\Table(name: 'easy_media__media')]
+class Media extends BaseMedia
+{
+}
